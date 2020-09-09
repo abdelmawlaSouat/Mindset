2d20d4e0dc456daf2ec3546dfb3d36d86b5d9a97
Abdelmawla <abdelmawla.souat@gmail.com>
2020-09-09 19:29:42 +0200

    MIN-01: create responsive HomePage template

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-toolbar flat>
+    <v-row align="center" justify="center" justify-sm="space-around">
+      <v-toolbar-title>
+        <v-img src="../assets/full-logo.png" alt="Logo" width="200" ></v-img>
+      </v-toolbar-title>
+      <div>
+        <v-btn v-show="!$vuetify.breakpoint.xsOnly" text large>Sign in</v-btn>
+        <v-btn v-show="!$vuetify.breakpoint.xsOnly" large color="info">Get Started</v-btn>
+      </div>
+    </v-row>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style lang="css">
+
+</style>
