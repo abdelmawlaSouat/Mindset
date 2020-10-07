19e13e522969341ca1d4efc99f7ce30dda73829a
Abdelmawla <abdelmawla.souat@gmail.com>
2020-10-07 10:08:11 +0200

    MIN-05: update user model (add new fields)

--- backend/models/User.js
+++ backend/models/User.js
@@ -10,36 +10,20 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-  googleID: {
-    type: String
-  },
-  facebookID: {
-    type: String
-  },
-  mailToken: {
-    type: String,
-    default: ''
-  },
-  displayName: {
-    type: String,
-    required: true
-  },
-  givenName: {
-    type: String
-  },
-  familyName: {
-    type: String
-  },
-  email: {
-    type: String
-  },
-  picture: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+  googleID: { type: String },
+  facebookID: { type: String },
+  mailToken: { type: String, default: '' },
+  displayName: { type: String, required: true },
+  name: { type: String },
+  givenName: { type: String },
+  familyName: { type: String },
+  email: { type: String },
+  bio: { type: String },
+  picture: { type: String },
+  newsletters: { type: Boolean, default: true },
+  socialNotification: { type: Boolean, default: true },
+  activated: { type: Boolean, default: true },
+  createdAt: { type: Date, default: Date.now }
 })
 
 module.exports = mongoose.model('User', UserSchema)
